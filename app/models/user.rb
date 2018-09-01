8f0affb174c606e51b4564a9996f1875ff7e599e
yusukeoyamada <fa19940118@gmail.com>
2018-09-01 20:58:01 +0900

    ログイン機能追加修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
   before_save{email.downcase!}
   validates :password, presence: true, length: {minimum: 6}
   has_secure_password
+
+  has_many :questions, dependent: :destroy
 end
