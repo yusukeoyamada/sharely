743c2b02d0d49f407b9faa635d483fc15d70a4bb
yusukeoyamada <fa19940118@gmail.com>
2018-09-26 00:40:11 +0900

    ユーザーモデルを微修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -72,7 +72,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, length:{maximum: 30}
   validates :email, presence: true, uniqueness: true, length:{maximum: 80} , format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
-  validates :password, presence: true, length: {minimum: 6}
 
   has_many :questions, dependent: :destroy
   has_many :reports, dependent: :destroy
